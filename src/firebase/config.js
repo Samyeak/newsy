76b5e23d16fefc66a6fa1f3873e6d90855fd7afe
Samyeak <10978827+Samyeak@users.noreply.github.com>
2020-06-20 17:12:43 +0545

    Firebase Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBOgFNfgCRAi_KCcBBDVHOKsrHCVOzyu90",
+    authDomain: "newsy-first-learn.firebaseapp.com",
+    databaseURL: "https://newsy-first-learn.firebaseio.com",
+    projectId: "newsy-first-learn",
+    storageBucket: "newsy-first-learn.appspot.com",
+    messagingSenderId: "878297234301",
+    appId: "1:878297234301:web:cc5474f6e357fb04ec2a14"
+  };
+
+
+  export default firebaseConfig;
